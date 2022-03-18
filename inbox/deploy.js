34d18bf93c5241df58ef7a30bd0ff5ef46686307
pat <dimuziop@gmail.com>
2022-03-18 17:45:46 +0100

    feature: deploy to a test network

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,1 +1,35 @@
-// deploy code will go here
+require('dotenv').config();
+
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require('web3');
+const {abi, evm} = require('./compile');
+
+const INITIAL_MESSAGE = 'Initial Message';
+const MNEMONIC = process.env.MNEMONIC;
+const INFURA_URL = process.env.INFURA_URL;
+
+const provider = new HDWalletProvider(
+    MNEMONIC,
+    INFURA_URL
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    const deploymentAccount = accounts[0]
+
+    console.log("Attempting to deploy from account", deploymentAccount);
+
+    const inbox = await new web3.eth.Contract(abi)
+        .deploy({ data: evm.bytecode.object, arguments: [INITIAL_MESSAGE] })
+        .send({ from: deploymentAccount, gas: 1000000 });
+
+    console.log("Contract deployed to:", inbox.options.address); //0xEf3141CA6C53142DbAD1bbF4DF61b21cb46d0472
+    provider.engine.stop();
+};
+
+deploy();
+
+
--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,5 +1,4 @@
 
-const { json } = require('@nomiclabs/buidler/internal/core/params/argumentTypes');
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
@@ -9,11 +8,12 @@ const web3 = new Web3(ganache.provider());
 
 let accounts;
 let inbox;
+const INITIAL_MESSAGE = 'Initial Message';
 
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
   inbox = await new web3.eth.Contract(abi)
-    .deploy({ data: evm.bytecode.object, arguments: ['Initial Message'] })
+    .deploy({ data: evm.bytecode.object, arguments: [INITIAL_MESSAGE] })
     .send({ from: accounts[0], gas: 1000000 });
 });
 
@@ -22,4 +22,22 @@ describe('Inbox', () => {
   it('deploys a contract', () => {
     assert.ok(inbox.options.address);
   });
+
+  it('has a default message ', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_MESSAGE)
+  });
+
+  it('should update the message', async () => {
+
+    const newMessage = "New Message";
+
+    const message = await inbox.methods.message().call();
+    await inbox.methods.setMessage(newMessage).send({ from: accounts[0], gas: 1000000 });
+    const updatedMessage = await inbox.methods.message().call();
+
+    assert.notEqual(message, updatedMessage)
+    assert.equal(updatedMessage, newMessage)
+
+  })
 });
