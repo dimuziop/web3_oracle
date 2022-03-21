10ef6f9bb4cb641895fb016c64f732802e69ff00
pat <dimuziop@gmail.com>
2022-03-21 16:56:49 +0100

    training: more contracts basic stuff

---
+++ lottery/contracts/Lottery.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.8.12;
+
+contract Lottery {
+
+    address public manager;
+    address[] public players;
+
+    constructor(){
+        manager = msg.sender;
+    }
+
+    function enter() public payable {
+        require(msg.value > .01 ether, "At least .01 ether is required");
+        players.push(msg.sender);
+    }
+
+    function pseudoRandom() private view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
+    }
+
+    function pickWinner() public restricted {
+        uint index = pseudoRandom() % players.length;
+        payable(players[index]).transfer(address(this).balance);
+        players = new address[](0);
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager, "Just owner executable");
+        _;
+    }
+
+    function getPlayers() public view returns (address[] memory){
+        return players;
+    }
+}
