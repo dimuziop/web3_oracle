d0a183da3dbb62fca03a613a498b86aba8f1b2f5
pat <dimuziop@gmail.com>
2022-03-23 12:17:47 +0100

    feature: adding frontend binding contract methods

--- lottery-client/src/App.js
+++ lottery-client/src/App.js
@@ -1,27 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
 import web3 from "./web3";
+import lottery from "./lottery";
+import {Component} from "react";
 
-function App() {
-  web3.eth.getAccounts().then(console.log)
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = {manager: "", players: [], balance: '', value: '', message: "", isManager: false}
+
+    async componentDidMount() {
+        const manager = await lottery.methods.manager().call();
+        const players = await lottery.methods.getPlayers().call();
+        const balance = await web3.eth.getBalance(lottery.options.address);
+
+        this.setState({manager})
+        this.setState({players})
+        this.setState({balance})
+        await this.isManager();
+    }
+
+    onSubmit = async (event) => {
+        event.preventDefault();
+        const accounts = await web3.eth.getAccounts();
+
+        this.setState({message: "Waiting on transaction success..."})
+
+        const tx = await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei(this.state.value, 'ether')
+        })
+
+        this.setState({message: "You have been entered, TX: " + tx.transactionHash })
+    }
+
+    onClick = async (event) => {
+        const accounts = await web3.eth.getAccounts();
+
+        this.setState({message: "Waiting on transaction success..."})
+
+        const tx = await lottery.methods.pickWinner().send({
+            from: accounts[0],
+        })
+
+        this.setState({message: "A winner have been picked, TX: " + tx.transactionHash })
+    }
+
+    isManager = async () => {
+        const accounts = await web3.eth.getAccounts();
+        this.setState({manager: this.state.manager === accounts[0]});
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h2>Lottery Contract</h2>
+                <p>
+                    This contract is managed by {this.state.manager}.
+                    There are currently {this.state.players.length} people in.
+                    The total amount is {web3.utils.fromWei(this.state.balance, 'ether')} Ethers!
+                </p>
+                <hr/>
+                <form onSubmit={this.onSubmit}>
+                    <h4>Want to try your luck?</h4>
+                    <div>
+                        <label>Amount of ether to enter</label>
+                        <input
+                            value={this.state.value}
+                            onChange={(event) => this.setState({ value: event.target.value })}
+                        />
+                    </div>
+                    <button>Enter</button>
+                </form>
+                <hr/>
+                <h4>Ready to pick a winner</h4>
+                <button onClick={this.onClick}>Pick a winner</button>
+                <hr />
+                <h2>{this.state.message}</h2>
+                <hr/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ lottery-client/src/lottery.js
@@ -0,0 +1,76 @@
+import Web3 from "./web3";
+
+const address = "0x0f9218d7931bBa89Ba0E9Fa98B5BD2D905Ac6D7e";
+const abi = [
+    {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [],
+        "name": "manager",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "players",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    },
+    {
+        "inputs": [],
+        "name": "enter",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function",
+        "payable": true
+    },
+    {
+        "inputs": [],
+        "name": "pickWinner",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getPlayers",
+        "outputs": [
+            {
+                "internalType": "address[]",
+                "name": "",
+                "type": "address[]"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function",
+        "constant": true
+    }
+];
+
+
+export default new Web3.eth.Contract(abi, address);
